cd46c2a49da347115d06ea14351c7559dabe49ac
eternityzqf <1583510917@qq.com>
2018-04-17 13:43:06 +0800

    modified login layout

--- app/src/main/java/com/yxcl/lpsenterprise/view/activity/login/Login.java
+++ app/src/main/java/com/yxcl/lpsenterprise/view/activity/login/Login.java
@@ -1,6 +1,7 @@
 package com.yxcl.lpsenterprise.view.activity.login;
 
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -27,22 +28,24 @@ public class Login extends BaseActivity<LoginPresenter> implements ILoginPresent
 
     @Bind(R.id.login_top_view)
     View view;
-    @Bind(R.id.account_image)
-    ImageView accountImage;
-    @Bind(R.id.account_qchu)
-    ImageView accountQchu;
-    @Bind(R.id.login_account_ed)
-    EditText loginAccountEd;
-    @Bind(R.id.pas_image)
-    ImageView pasImage;
-    @Bind(R.id.password_qchu)
-    ImageView passwordQchu;
-    @Bind(R.id.login_psw_ed)
-    EditText loginPswEd;
-    @Bind(R.id.remember_psw_cb)
-    CheckBox rememberPswCb;
-    @Bind(R.id.login_btn)
-    Button loginBtn;
+    @Bind(R.id.new_account_qchu)
+    ImageView newAccountQchu;
+    @Bind(R.id.new_login_account_ed)
+    EditText newLoginAccountEd;
+    @Bind(R.id.new_password_qchu)
+    ImageView newPasswordQchu;
+    @Bind(R.id.new_login_psw_ed)
+    EditText newLoginPswEd;
+    @Bind(R.id.new_code_img)
+    ImageView newCodeImg;
+    @Bind(R.id.new_code_qchu)
+    ImageView newCodeQchu;
+    @Bind(R.id.new_login_code_ed)
+    EditText newLoginCodeEd;
+    @Bind(R.id.new_remember_psw_cb)
+    CheckBox newRememberPswCb;
+    @Bind(R.id.new_login_btn)
+    Button newLoginBtn;
 
     @Override
     protected LoginPresenter createPresenter() {
@@ -51,31 +54,35 @@ public class Login extends BaseActivity<LoginPresenter> implements ILoginPresent
 
     @Override
     protected int provideContentViewId() {
-        return R.layout.login_layout;
+        return R.layout.new_login_layout;
     }
 
     @Override
     public void initView() {
         super.initView();
-    }
-
-    @Override
-    protected void initImmersionBar() {
-        super.initImmersionBar();
-        mImmersionBar.statusBarView(view).init();
         ImmersionBar.with(this).statusBarDarkFont(true, 0.2f).init();
+        //让布局向上移来显示软键盘
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);//让布局向上移来显示软键盘
     }
 
-    @OnClick({R.id.account_qchu, R.id.password_qchu, R.id.login_btn})
+    @OnClick({R.id.new_account_qchu, R.id.new_password_qchu
+            , R.id.new_code_img, R.id.new_code_qchu, R.id.new_login_btn})
     public void onViewClicked(View view) {
         switch (view.getId()) {
-            case R.id.account_qchu:
-
+            case R.id.new_account_qchu:
+                //账号清除
                 break;
-            case R.id.password_qchu:
-
+            case R.id.new_password_qchu:
+                //密码
                 break;
-            case R.id.login_btn:
+            case R.id.new_code_img:
+                //点击获取验证码
+                break;
+            case R.id.new_code_qchu:
+                //密码清除
+                break;
+            case R.id.new_login_btn:
+                //登录
                 Util.NextActivity(this, RadioMainActivity.class);
                 break;
         }
